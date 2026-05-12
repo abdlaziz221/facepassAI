390a422cdec270bc8e225332361b42f09697da15
Souleymane Sirima Mbodj <sirimambodj@gmail.com>
2026-05-12 15:32:18 +0000

    test: nettoyage tests Breeze obsoletes apres customizations Sprint 1-3

--- facepassAI/database/factories/UserFactory.php
+++ facepassAI/database/factories/UserFactory.php
@@ -25,11 +25,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name'              => fake()->name(),
+            'email'             => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password'          => static::$password ??= Hash::make('password'),
+            'remember_token'    => Str::random(10),
+            'est_actif'         => true,  // Sprint 1 T8 : CheckAccountActive middleware
         ];
     }
 
@@ -40,6 +41,16 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the user account is deactivated.
+     */
+    public function inactif(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'est_actif' => false,
         ]);
     }
 }
